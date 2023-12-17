638e3290d33e9d27bf6b127dd1fa49da6cb01013
“Colin <muriithicolin98@gmail.com>
2023-12-17 14:09:47 +0300

    Updates

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="fixed top-0 left-0 w-16 m-0 h-screen flex flex-col bg-gray-900 shadow-xl ">
+    <div className="fixed top-0 left-0 w-16 m-0 h-screen flex flex-col bg-gray-900 text-white shadow-lg">
       <i>A</i>
       <i>B</i>
       <i>C</i>
