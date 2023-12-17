6806ed6526af526eed8796472f87643228734545
“Colin <muriithicolin98@gmail.com>
2023-12-17 22:29:00 +0300

    Side Bar Done!

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { BsAmazon, BsLightningFill } from "react-icons/bs";
+import { FaPoop, FaFire } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
-    <div className="fixed top-0 left-0 w-16 m-0 h-screen flex flex-col bg-gray-900 text-white shadow-lg">
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-      <i>E</i>
+    <>
+      <div
+        className="fixed top-0 left-0 w-16 m-0 h-screen p-1
+    flex flex-col bg-primary text-secondary shadow-lg"
+      >
+        <SidebarIcon icon={<FaFire size={28} />} />
+        <SidebarIcon icon={<FaPoop size={20} />} />
+        <SidebarIcon icon={<BsAmazon size={30} />} />
+      </div>
+      <div className="fixed bottom-3 left-0 mx-auto p-2 text-secondary ">
+        <SidebarIcon icon={<BsLightningFill size={26} />} />
+      </div>
+    </>
+  );
+};
+
+const SidebarIcon = ({ icon, text = "tooltip 💡" }) => {
+  return (
+    <div className="sidebar-icon group">
+      {icon}
+      <span class="sidebar-tooltip group-hover:scale-100">{text}</span>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#202225",
+        secondary: "#5865f2",
+      },
+    },
     plugins: [],
   },
 };
