7f27994178825293cebc2fbd817d5e971d29768c
“Colin <muriithicolin98@gmail.com>
2023-12-30 19:38:42 +0300

    Updates

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -3,14 +3,19 @@ import React from "react";
 const Dashboard = () => {
   return (
     <div className=" fixed top-[-1] bottom-1 left-14 right-1 h-[91%] w-[90%] bg-slate-500 rounded-xl m-3 px-3 py-5 flex justify-center items-center">
-      <p className="text-3xl absolute top-3 left-4">Lender Dashboard</p>
+      <p className="text-3xl absolute top-3 left-4">Lender Admin</p>
+      <button className="text-gl absolute top-3 right-4 bg-red-600 rounded-md px-2 py-1">
+        Invite Employee
+      </button>
       <div className="absolute flex m-10 p-2 top-3 left-4">
         <p className="px-2">Dashboard</p>
         <p className="px-2">Employees</p>
       </div>
-      <div>
-        
+      <div className=" absolute flex mx-2 top-16 right-5 px-2">
+        <p className="px-3">Reset User Password</p>
+        <input type="text" placeholder="Search" />
       </div>
+      <div></div>
     </div>
   );
 };
