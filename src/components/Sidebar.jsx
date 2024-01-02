41e112c82f71e715bcd8a5f1af70858f7ab98d7e
“Colin <muriithicolin98@gmail.com>
2024-01-02 23:14:38 +0300

    Updates

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { BsAmazon, BsLightningFill } from "react-icons/bs";
 import { FaPoop, FaFire } from "react-icons/fa";
+import { ReactComponent as HomeIcon } from "../icons/home.svg";
+import { ReactComponent as MenuIcon } from "../icons/menu.svg";
+import { ReactComponent as ButsIcon } from "../icons/buts.svg";
+import { ReactComponent as LogoutIcon } from "../icons/upload.svg";
 
 const Sidebar = () => {
   return (
@@ -9,12 +13,12 @@ const Sidebar = () => {
         className="fixed top-0 left-0 w-16 m-0 h-screen p-1
     flex flex-col bg-primary text-secondary shadow-lg"
       >
-        <SidebarIcon icon={<FaFire size={30} />} />
-        <SidebarIcon icon={<FaPoop size={25} />} />
-        <SidebarIcon icon={<BsAmazon size={30} />} />
+        <SidebarIcon icon={<HomeIcon size={30} />} />
+        <SidebarIcon icon={<MenuIcon size={25} />} />
+        <SidebarIcon icon={<ButsIcon size={30} />} />
       </div>
       <div className="fixed bottom-3 left-0 mx-auto p-2 text-secondary ">
-        <SidebarIcon icon={<BsLightningFill size={26} />} />
+        <SidebarIcon icon={<LogoutIcon size={26} />} />
       </div>
     </>
   );
