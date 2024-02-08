eaaf327b124be4217fc2966fb6000902b4f5d75d
“Colin <muriithicolin98@gmail.com>
2024-02-08 20:39:31 +0300

    Back at it😎

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,29 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { ReactComponent as PlusIcon } from "./icons/plus-circle.svg";
 
 const Dashboard = () => {
+  const [transactions, setTransactions] = useState("1,598");
+  const [pendingApp, setPendingApp] = useState(0);
+  const [completedApp, setCompletedApp] = useState(0);
+
   return (
-    <div className=" fixed top-[-1] bottom-1 left-14 right-1 h-[91%] w-[90%] bg-slate-500 rounded-xl m-3 px-3 py-5 flex justify-center items-center">
-      <p className="text-3xl absolute top-3 left-4">Lender Admin</p>
-      <button className="text-gl absolute top-3 right-4 bg-red-600 rounded-md px-2 py-1">
-        Invite Employee
+    <div className=" fixed top-[-1] bottom-1 left-14 right-1 h-[91%] w-[90%] bg-gray-800 rounded-xl m-3 px-3 py-5 flex justify-center items-center font-arial">
+      <p className="text-3xl text-white absolute top-3 left-11">Lender Admin</p>
+      <button className=" flex items-center text-sm absolute top-3 right-4 bg-red-600 rounded-md px-4 py-2">
+        <PlusIcon className="mr-2" />
+        <div>Invite Employee</div>
       </button>
-      <div className="absolute flex m-10 p-2 top-3 left-4">
-        <p className="px-2">Dashboard</p>
-        <p className="px-2">Employees</p>
+      <div className="absolute flex my-12 mx-7 p-2 top-3 left-1">
+        <p className="px-2 mx-1 rounded-md bg-gray-600">Dashboard</p>
+        <p className="px-2 mx-1 rounded-md bg-gray-600">Employees</p>
       </div>
-      <div className=" absolute flex mx-2 top-16 right-5 px-2">
+      {/* <div className=" absolute flex mx-2 top-16 right-5 px-2">
         <p className="px-3">Reset User Password</p>
-        <input type="text" placeholder="Search" />
-      </div>
-      <div className="w-full  grid grid-flow-col grid-cols-4">
-        <div className="relative top-[10%] rounded-md bg-blue-400 text-white px-[40px] py-[200px]">
-          1
+        <input type="text" placeholder="Search" className="rounded-lg" />
+      </div> */}
+      <div className="w-full  grid grid-flow-col grid-cols-3 px-8">
+        <div className="relative top-[10%] rounded-md bg-gray-700 text-white px-[40px] py-[240px] mr-2">
+          <div className="absolute top-2 text-sm left-2 text-blue-400">
+            Transactions
+          </div>
+          <div className="absolute bottom-2 right-2 text-7xl">
+            {transactions}
+          </div>
         </div>
-        <div className="relative top-[10%] rounded-md bg-blue-400 text-white p-6 px-[40px] py-[200px]">
-          2
+        <div className="relative top-[10%] rounded-md bg-gray-700 text-white p-6 px-[40px] py-[200px] mr-2">
+          <div className="absolute top-2 text-sm left-2 text-blue-400">
+            Applicants Pending
+          </div>
+          <div className="absolute bottom-2 right-2 text-7xl">{pendingApp}</div>
         </div>
-        <div className="relative top-[10%] rounded-md bg-blue-400 text-white p-6 px-[40px] py-[200px]">
-          3
+        <div className="relative top-[10%] rounded-md bg-gray-700 text-white p-6 px-[40px] py-[200px] mr-2">
+          <div className="absolute top-2 text-sm left-2 text-blue-400">
+            Applicants Completed
+          </div>
+          <div className="absolute bottom-2 right-2 text-7xl">
+            {completedApp}
+          </div>
         </div>
       </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { BsAmazon, BsLightningFill } from "react-icons/bs";
-import { FaPoop, FaFire } from "react-icons/fa";
 import { ReactComponent as HomeIcon } from "../icons/home.svg";
 import { ReactComponent as MenuIcon } from "../icons/menu.svg";
 import { ReactComponent as ButsIcon } from "../icons/buts.svg";
@@ -9,22 +7,28 @@ import { ReactComponent as LogoutIcon } from "../icons/upload.svg";
 const Sidebar = () => {
   return (
     <>
+      <div className="fixed top-3 right-12 text-gray-500 text-sm">
+        muriithicolin98@gmail...
+      </div>
       <div
         className="fixed top-0 left-0 w-16 m-0 h-screen p-1
     flex flex-col bg-black text-secondary shadow-lg"
       >
-        <SidebarIcon icon={<HomeIcon size={30} />} />
-        <SidebarIcon icon={<MenuIcon size={25} />} />
-        <SidebarIcon icon={<ButsIcon size={30} />} />
+        <SidebarIcon icon={<HomeIcon size={30} text={"Home"} />} />
+        <SidebarIcon icon={<ButsIcon size={30} text={"Dashboard"} />} />
+        <SidebarIcon
+          icon={<MenuIcon size={25} text={"Employees"} className="bg-black" />}
+        />
       </div>
-      <div className="fixed bottom-3 left-0 mx-auto p-2 text-secondary ">
-        <SidebarIcon icon={<LogoutIcon size={26} />} />
+      <div className="fixed bottom-3 left-0 mx-auto px-4 my-2 text-gray-600 font-arial text-xs ">
+        <SidebarIcon icon={<LogoutIcon size={22} text={"Logout"} />} />
+        <p className="">Log Out</p>
       </div>
     </>
   );
 };
 
-const SidebarIcon = ({ icon, text = "tooltip 💡" }) => {
+const SidebarIcon = ({ icon, text }) => {
   return (
     <div className="sidebar-icon group">
       {icon}
